0c42e9dc8faeeb31895c48497c7a5eda37e0bde4
guchen <1023797278@qq.com>
2023-06-04 18:33:23 +0800

    Alter name

--- src/views/EventDetail.vue
+++ src/views/EventDetail.vue
@@ -15,7 +15,7 @@
                 </n-list-item>
                 <n-list-item>
                     <n-skeleton v-if="loading" text style="width:200px" />
-                    <div v-else><span style="font-weight:800;padding-right:15px">Src Agent:</span><n-a
+                    <div v-else><span style="font-weight:800;padding-right:15px">Source Agent:</span><n-a
                             @click="router.push('/agents?id=' + event.srcAgentId)">{{ event.srcAgentName }}</n-a></div>
                 </n-list-item>
                 <n-list-item>
